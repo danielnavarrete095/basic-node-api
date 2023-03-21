977693f0c5dedadb68d9bbf34b14b913bb9a7385
Daniel Navarrete <danielnavarrete095@gmail.com>
2023-03-20 19:20:21 -0600

    Add express router for clients

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import {getClients, getClient, addClient, updateClient, deleteClient} from './database.js';
 import bodyParser from 'body-parser';
+import clientsRoutes from './routes/clients.routes.js'
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,45 +12,7 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 })
 
-app.get('/clients', async (req, res) => {
-    const clients = await getClients();
-    res.send(clients);
-})
-
-app.get('/clients/:id', async (req, res) => {
-    const clientId = req.params.id;
-    const client = await getClient(clientId);
-    if(client !== undefined) {
-        res.send(client);
-    } else {
-        res.status(404).end(`Client ${clientId} not found`);
-        console.log(client);
-    }
-})
-
-app.post('/add', async (req, res) => {
-    const clientId = req.params.id;
-    const {name, city} = req.body;
-    const result = await addClient(name, city);
-    res.send(result);
-    // res.send(`Error adding client ${clientId}`)
-})
-
-app.put('/update/:id', async (req, res) => {
-    const clientId = req.params.id;
-    const {name, city} = req.body;
-    const result = await updateClient(clientId, name, city);
-    res.send(result);
-    // res.send(`Client with id: ${clientId} updated`)
-})
-
-
-app.delete('/delete/:id', async (req, res) => {
-    const clientId = req.params.id;
-    const result = await deleteClient(clientId);
-    res.send(result);
-    // res.send(`Error deleting ${clientId}`);
-})
+app.use(clientsRoutes);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
---
+++ src/routes/clients.routes.js
@@ -0,0 +1,46 @@
+import {Router} from 'express'
+import {getClients, getClient, addClient, updateClient, deleteClient} from '../database.js';
+
+const router = Router();
+
+router.get('/clients', async (req, res) => {
+    const clients = await getClients();
+    res.send(clients);
+});
+
+router.get('/clients/:id', async (req, res) => {
+    const clientId = req.params.id;
+    const client = await getClient(clientId);
+    if(client !== undefined) {
+        res.send(client);
+    } else {
+        res.status(404).end(`Client ${clientId} not found`);
+        console.log(client);
+    }
+});
+
+router.post('/clients', async (req, res) => {
+    const clientId = req.params.id;
+    const {name, city} = req.body;
+    const result = await addClient(name, city);
+    res.send(result);
+    // res.send(`Error adding client ${clientId}`)
+});
+
+router.put('/clients/:id', async (req, res) => {
+    const clientId = req.params.id;
+    const {name, city} = req.body;
+    const result = await updateClient(clientId, name, city);
+    res.send(result);
+    // res.send(`Client with id: ${clientId} updated`)
+});
+
+
+router.delete('/delete/:id', async (req, res) => {
+    const clientId = req.params.id;
+    const result = await deleteClient(clientId);
+    res.send(result);
+    // res.send(`Error deleting ${clientId}`);
+});
+
+export default router;
