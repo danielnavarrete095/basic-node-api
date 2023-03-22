dc7f19927eeee859b3349fffb43c4a25e386bad9
Daniel Navarrete <danielnavarrete095@gmail.com>
2023-03-21 23:19:14 -0600

    Add log for every HTTP request

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -4,12 +4,14 @@ import {getClients, getClient, addClient, updateClient, deleteClient} from '../d
 const router = Router();
 
 router.get('/clients', async (req, res) => {
+    console.log('Incoming GET request');
     const clients = await getClients();
     console.log(clients);
     res.send(clients);
 });
 
 router.get('/clients/:id', async (req, res) => {
+    console.log('Incoming GET request');
     const clientId = req.params.id;
     const client = await getClient(clientId);
     if(client !== undefined) {
@@ -22,6 +24,7 @@ router.get('/clients/:id', async (req, res) => {
 });
 
 router.post('/clients', async (req, res) => {
+    console.log('Incoming POST request');
     const {name, city} = req.body;
     console.log(`Add client\nname: ${name}, city: ${city}`);
     const result = await addClient(name, city);
@@ -30,6 +33,7 @@ router.post('/clients', async (req, res) => {
 });
 
 router.put('/clients/:id', async (req, res) => {
+    console.log('Incoming PUT request');
     const clientId = req.params.id;
     const {name, city} = req.body;
     const result = await updateClient(clientId, name, city);
@@ -39,6 +43,7 @@ router.put('/clients/:id', async (req, res) => {
 
 
 router.delete('/clients/:id', async (req, res) => {
+    console.log('Incoming DELETE request');
     const clientId = req.params.id;
     const result = await deleteClient(clientId);
     console.log(result);
